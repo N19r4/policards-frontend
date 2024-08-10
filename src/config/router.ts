de069cd9e4801ecf6491a688dd613ae7414bb82a
Kinga Grabarczyk <kinga.grabarczyk@euvic.pl>
2024-08-10 18:56:13 +0200

    fix: corrected routes.

--- src/config/router.ts
+++ src/config/router.ts
@@ -2,8 +2,8 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import { FlashcardSetsView, LoginView, SignUpView } from '../views'
 
 const routes = [
-  { path: '/', component: SignUpView },
-  { path: '/login', component: LoginView },
+  { path: '/', component: LoginView },
+  { path: '/sign-up', component: SignUpView },
   { path: '/flashcard-sets', component: FlashcardSetsView},
 ]
 
