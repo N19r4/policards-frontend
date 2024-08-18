34a3f25a24cb586026f746036c774c58ce597ec7
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2024-08-18 14:53:59 +0200

    added landig page view with routing

--- src/config/router.ts
+++ src/config/router.ts
@@ -3,11 +3,13 @@ import { MainHubView } from '../views'
 import { FlashcardSetsView } from '../views'
 import LoginView from '../views/LoginView.vue'
 import SignUpView from '../views/SignUpView.vue'
+import LandingView from '../views/LandingView.vue'
 
 const routes = [
   { path: '/login', component: () => LoginView },
-  {path: '/', component: () => SignUpView},
-  { path: '/flashcard-sets', component: () => FlashcardSetsView },
+  { path: '/', component: () => LandingView},
+  { path: '/sign-up', component: () => SignUpView},
+  { path: '/flashcard-sets', component: () => FlashcardSetsView }
 ]
 
 export const router = createRouter({
